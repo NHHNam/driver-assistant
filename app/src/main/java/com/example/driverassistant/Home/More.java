58f0b00812b0e7c88180dc1c0c41a083ee05f77f
Pham Hong Duc <iamphduc@gmail.com>
2021-04-30 22:10:02 +0700

    Duc - refactor home

--- app/src/main/java/com/example/driverassistant/Home/More.java
+++ app/src/main/java/com/example/driverassistant/Home/More.java
@@ -1,4 +1,4 @@
-package com.example.driverassistant.More;
+package com.example.driverassistant.Home;
 
 import android.os.Bundle;
 
@@ -11,6 +11,6 @@ public class More extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.more_home);
+        setContentView(R.layout.home_more);
     }
 }
